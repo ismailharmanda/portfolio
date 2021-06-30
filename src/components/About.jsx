55adbc7bf095ef2c0c2b049cf3ad21f180525f7c
ismail harmanda <iharmanda@hotmail.com>
2021-06-30 10:49:59 +0300

    pdf embed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,36 @@
 import React from "react";
+import { Document } from "react-pdf";
 
-const About = ()=>{
-return(
+const About = () => {
+  return (
     <div className="row">
-    <section className="" id="aboutme" name="about">
-    <div id="intro">
-      <div className="">
-        <div className="row align-items-center justify-content-lg-center">
-
-          <div className="col-lg-3 d-flex justify-content-center ">
-            <h5 className="h1 text-light">ABOUT ME</h5>
+      <section className="" id="aboutme" name="about">
+        <div id="intro">
+          <div className="">
+            <div className="row align-items-center justify-content-lg-center">
+              <div className="col-lg-3 d-flex justify-content-center ">
+                <h5 className="h1 text-light">ABOUT ME</h5>
+              </div>
+              <div className="col-lg-6 text-light">
+                <p>
+                  Hi there✋. I am an eager and hardworker developer. My main
+                  career goal is to learn every day. I really want to learn and
+                  to progress in my career. Programming requires constant
+                  learning and improving.
+                </p>
+              </div>
+              <div className="col-lg-3 justify-content.center">
+                <embed
+                  src="https://docdro.id/bnfgb2j"
+                  width="300px"
+                  height="400px"
+                />
+              </div>
+            </div>
           </div>
-          <div className="col-lg-6 text-light">
-            <p>Hi there✋. I am an eager and hardworker developer. My main career goal is to learn every day. I
-really want to learn and to progress in my career. Programming requires
-constant learning and improving.</p>
-          </div>
-          <div className="col-lg-3 justify-content.center">
-          <iframe className="scribd_iframe_embed" title="İsmail Harmanda CV" src="https://www.scribd.com/embeds/513530039/content?start_page=1&view_mode=scroll&access_key=key-79goHlk6jEuIKDdUCJFg" data-auto-height="false" data-aspect-ratio="0.7074509803921568" scrolling="no" id="doc_54980" width="400" height="533" frameBorder="0"></iframe>
-          </div>
-
         </div>
-      </div>
+      </section>
     </div>
-  </section>
-    </div>
-)
-}
+  );
+};
 export default About;
