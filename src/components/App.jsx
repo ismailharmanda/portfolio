10019eae4e7c734a1a39f87dd8fea28556fe146c
ismail harmanda <iharmanda@hotmail.com>
2021-06-30 14:27:12 +0300

    Comment componenet has been done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,18 @@ import Navbar from "./Navbar";
 import Section from "./Section";
 import About from "./About";
 import Works from "./Works";
-import "../css/App.css"
+import "../css/App.css";
 import Comment from "./Comment";
 
-const App = ()=>{
-    return(
-        <div className="">
-        <Navbar />
-        <Section />
-        <About />
-        <Works />
-        <Comment />
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <Section />
+      <About />
+      <Works />
+      <Comment />
+    </div>
+  );
+};
 export default App;
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,27 +1,130 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-const Comment=(props)=>{
-    const [text,setText]=useState("")
-    return(
-<div>
-<section className="row bg-dark text-light">
-<div className="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
-                <form id="algin-form">
-                    <div className="form-group">
-                        <h4>Leave a comment</h4> <label for="message">Message</label> <textarea name="msg" id="" msg cols="30" rows="5" className="form-control" style={{backgroundColor: "black"}}></textarea>
-                    </div>
-                    <div className="form-group"> <label for="name">Name</label> <input type="text" name="name" id="fullname" className="form-control" /> </div>
-                    <div className="form-group"> <label for="email">Email</label> <input type="text" name="email" id="email" className="form-control" /> </div>
-                    <div className="form-group">
-                        <p className="text-secondary">If you have a <a href="#" className="alert-link">gravatar account</a> your address will be used to display your profile picture.</p>
-                    </div>
-                    <div className="form-inline"> <input type="checkbox" name="check" id="checkbx" className="mr-1" /> <label for="subscribe">Subscribe me to the newlettter</label> </div>
-                    <div className="form-group"> <button type="button" id="post" className="btn">Post Comment</button> </div>
-                </form>
+const Comment = (props) => {
+  const [text, setText] = useState({ message: "", name: "", email: "" });
+  const [comments, setComments] = useState([]);
+  const onButtonClick = (e) => {
+    e.preventDefault();
+    setComments([...comments, text]);
+    setText({ message: "", name: "", email: "" });
+  };
+  const deleteComment = (e) => {
+    return setComments(
+      comments.filter((comment, index) => {
+        console.log(index);
+        console.log(e.target.id);
+        return index !== Number(e.target.id);
+      })
+    );
+  };
+  const allComments =
+    comments.length > 0 &&
+    comments.map((comment, index) => {
+      return (
+        <div key={index}>
+          <div className="row align-items-center">
+            <div className="col-10 bg-light text-dark comment mt-4 text-justify float-left">
+              <h4>
+                Name: {comment.name} Email: {comment.email}
+              </h4>
+              <br />
+              <p>{comment.message}</p>
             </div>
-</section>
-</div>
-)
-}
+            <div className="col-1">
+              <i
+                style={{ cursor: "pointer" }}
+                id={index}
+                onClick={(e) => deleteComment(e)}
+                title="Delete"
+                className="fa fa-trash-alt inline"
+              ></i>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  return (
+    <div id="comments" className="row bg-dark text-light">
+      <div className="col-md-6">{allComments}</div>
+      <div className="col-md-6">
+        <section className="">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            id="algin-form"
+          >
+            <div className="form-group">
+              <h4>Leave a comment</h4> <label for="message">Message</label>{" "}
+              <textarea
+                onChange={(e) =>
+                  setText({
+                    message: e.target.value,
+                    name: text.name,
+                    email: text.email,
+                  })
+                }
+                value={text.message}
+                name="message"
+                id=""
+                cols="30"
+                rows="5"
+                className="form-control bg-white"
+              ></textarea>
+            </div>
+            <div className="form-group">
+              {" "}
+              <label htmlFor="name">Name</label>{" "}
+              <input
+                onChange={(e) =>
+                  setText({
+                    name: e.target.value,
+                    message: text.message,
+                    email: text.email,
+                  })
+                }
+                value={text.name}
+                type="text"
+                name="name"
+                id="fullname"
+                className="form-control"
+              />{" "}
+            </div>
+            <div className="form-group">
+              {" "}
+              <label htmlFor="email">Email</label>{" "}
+              <input
+                onChange={(e) =>
+                  setText({
+                    email: e.target.value,
+                    message: text.message,
+                    name: text.name,
+                  })
+                }
+                value={text.email}
+                type="text"
+                name="email"
+                id="email"
+                className="form-control"
+              />{" "}
+            </div>
+            <div className="form-inline mt-2">
+              {" "}
+              <button
+                onClick={(e) => {
+                  onButtonClick(e);
+                }}
+                type="button"
+                className="btn btn-light fw-bold"
+              >
+                Send
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Comment;
