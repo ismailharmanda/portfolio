0c3b38ae68f98b868d69363e6ab5c0262ead165e
ismail harmanda <iharmanda@hotmail.com>
2021-06-30 17:41:01 +0300

    navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,71 @@
 import React from "react";
 
-const Navbar = (props)=>{
-    return(
-        <div className="">
-        <header className=" header fixed-top p-0 m-0">
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark shadow-sm py-3 justify-content-md-center">
-        <div className="">
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+const Navbar = (props) => {
+  return (
+    <div className="">
+      <header className=" header fixed-top p-0 m-0">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow-sm py-3 justify-content-sm-center">
+          <div className="">
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link scroll-link" title="About Me" href="#aboutme"><i className="fa fa-user fa-3x"></i><span className="sr-only"></span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link scroll-link" title="Works" href="#works"><i className="fa fa-briefcase fa-3x"></i></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link scroll-link" title="Comments" href="#comments"><i className="fa fa-comments fa-3x"></i></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link scroll-link" title="Contact" href="#contact"><i className="fa fa-envelope fa-3x"></i></a>
-                    </li>
-                </ul>
+            <div
+              className="collapse navbar-collapse justify-content-center"
+              id="navbarSupportedContent"
+            >
+              <ul className="p-3 navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a
+                    className="nav-link scroll-link"
+                    title="About Me"
+                    href="#aboutme"
+                  >
+                    <i className="fa fa-user fa-3x"></i>
+                    <span className="sr-only"></span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link scroll-link"
+                    title="Works"
+                    href="#works"
+                  >
+                    <i className="fa fa-briefcase fa-3x"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link scroll-link"
+                    title="Comments"
+                    href="#comments"
+                  >
+                    <i className="fa fa-comments fa-3x"></i>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link scroll-link"
+                    title="Contact"
+                    href="#contact"
+                  >
+                    <i className="fa fa-envelope fa-3x"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
-        </div>
-    </nav>
-</header>
-        </div>
-    )
-}
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
 export default Navbar;
